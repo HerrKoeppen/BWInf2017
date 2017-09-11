f4a0e4f1f6cd13e3168f3d0db7783501995c0cac
koeppen <koeppen@r116-11.ad.b-cw-hwos.logodidact.net>
2017-09-11 09:12:28 +0200

    initial release

---
+++ src/junior2/Wintervorrat.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package junior2;
+
+/**
+ *
+ * @author koeppen
+ */
+public class Wintervorrat {
+    
+}
